4f02ade577f4f83b161081dd8ff056639c57f152
Pauli Karpanoja (pauli karpanoja <Pauli Karpanoja (pauli.karpanoja@solita.fi)>
2012-12-18 14:15:47 +0200

    Added populatemunicipalities.sql

--- municipalityinitiative-web/src/main/java/fi/om/municipalityinitiative/dao/JdbcMunicipalityInitiativeDao.java
+++ municipalityinitiative-web/src/main/java/fi/om/municipalityinitiative/dao/JdbcMunicipalityInitiativeDao.java
@@ -1,24 +1,51 @@
 package fi.om.municipalityinitiative.dao;
 
+import com.mysema.query.sql.dml.SQLInsertClause;
 import com.mysema.query.sql.postgres.PostgresQueryFactory;
 import fi.om.municipalityinitiative.dto.MunicipalityInitiativeCreateDto;
 import fi.om.municipalityinitiative.sql.QMunicipalityInitiative;
+import org.springframework.transaction.annotation.Transactional;
+
+import static fi.om.municipalityinitiative.sql.QMunicipalityInitiative.municipalityInitiative;
 
 import javax.annotation.Resource;
 
 @SQLExceptionTranslated
-public class JdbcMunicipalityInitiativeDao implements  MunicipalityInitiativeDao{
+public class JdbcMunicipalityInitiativeDao implements MunicipalityInitiativeDao {
 
     @Resource
     PostgresQueryFactory queryFactory;
 
     @Override
+    @Transactional(readOnly = true)
     public QMunicipalityInitiative find() {
         return null;
     }
 
     @Override
+    @Transactional(readOnly = false)
     public QMunicipalityInitiative create(MunicipalityInitiativeCreateDto dto) {
+        SQLInsertClause insert = queryFactory.insert(municipalityInitiative);
+
+        setInitiativeBasicInfo(dto, insert);
+        setContactInfo(dto, insert);
+
+        Long createId = insert.executeWithKey(municipalityInitiative.id);
+
         return null;
+
+    }
+
+    private void setContactInfo(MunicipalityInitiativeCreateDto dto, SQLInsertClause insert) {
+        insert.set(municipalityInitiative.contactAddress, dto.contactAddress);
+        insert.set(municipalityInitiative.contactEmail, dto.contactEmail);
+        insert.set(municipalityInitiative.contactPhone, dto.contactPhone);
+        insert.set(municipalityInitiative.contactName, dto.contactName);
+    }
+
+    private void setInitiativeBasicInfo(MunicipalityInitiativeCreateDto dto, SQLInsertClause insert) {
+        insert.set(municipalityInitiative.name, dto.name);
+        insert.set(municipalityInitiative.proposal, dto.proposal);
+        insert.set(municipalityInitiative.municipalityId, -5L);
     }
 }
--- municipalityinitiative-web/src/test/java/fi/om/municipalityinitiative/dao/JdbcMunicipalityInitiativeDaoTest.java
+++ municipalityinitiative-web/src/test/java/fi/om/municipalityinitiative/dao/JdbcMunicipalityInitiativeDaoTest.java
@@ -1,6 +1,10 @@
 package fi.om.municipalityinitiative.dao;
 
+import com.mysema.query.sql.dml.SQLInsertClause;
 import fi.om.municipalityinitiative.conf.NEWIntegrationTestConfiguration;
+import fi.om.municipalityinitiative.dto.MunicipalityInitiativeCreateDto;
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
@@ -15,8 +19,33 @@ public class JdbcMunicipalityInitiativeDaoTest {
     @Resource
     MunicipalityInitiativeDao municipalityInitiativeDao;
 
+    @Resource
+    NEWTestHelper testHelper;
+
+    @Before
+    public void setup() {
+        testHelper.dbCleanup();
+    }
+
     @Test
     public void dummyTest() {
         System.out.println("moi");
+    }
+
+    @Test
+    public void testCreate() {
+        MunicipalityInitiativeCreateDto dto = new MunicipalityInitiativeCreateDto();
+
+        dto.name = "initiative name";
+        dto.proposal = "initiative proposal";
+
+        dto.contactAddress = "contact address";
+        dto.contactName = "contact name";
+        dto.contactEmail = "contact email";
+        dto.contactPhone = "contact phone";
+
+        municipalityInitiativeDao.create(dto);
+
+
     }
 }
