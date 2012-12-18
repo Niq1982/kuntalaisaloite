14a167b47c46798390646b9c65a82f90acc9174b
Pauli Karpanoja (pauli karpanoja <Pauli Karpanoja (pauli.karpanoja@solita.fi)>
2012-12-18 09:18:58 +0200

    Removed more files

---
+++ municipalityinitiative-web/etc/create_muninit_test_schema.sh
@@ -0,0 +1,42 @@
+# NOTE: Set passwords in ~/.pgpass, e.g.
+# localhost:5432:*:postgres:postgres
+# localhost:5432:*:muninitest:1nitest
+
+if [ ! -f muninit_schema/01_schema.sql ]
+then
+  echo "schema/01_schema.sql not found. Execute this script in it's own folder!"
+  exit 0
+fi
+
+echo "== create_test_schema.sh =="
+
+export PGCLIENTENCODING="UTF8"
+
+# Drop/create schema 
+psql -U postgres -d muninitdb <<EOF
+DROP SCHEMA IF EXISTS muninitest CASCADE;
+DROP USER IF EXISTS muninitest;
+
+CREATE SCHEMA muninitest;
+CREATE USER muninitest WITH PASSWORD '1nitest';
+\q
+EOF
+
+# Create tables 
+export PGOPTIONS='--client-min-messages=warning --search-path=muninitest'
+
+ls muninit_schema/*.sql | sort -f |
+  while read file
+  do
+    echo "-- $file"
+    psql -U postgres -d muninitdb --single-transaction -f "$file"
+  done
+
+# Grant required rights
+psql -U postgres -d muninitdb <<EOF
+GRANT CONNECT, TEMP ON DATABASE muninitdb TO muninitest;
+GRANT USAGE ON SCHEMA muninitest TO muninitest;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA muninitest TO muninitest;
+GRANT ALL ON ALL SEQUENCES IN SCHEMA  muninitest TO muninitest;
+\q
+EOF
