b05bce8f9528ecc432150978bc540a2e1e3fa515
Pauli Karpanoja (pauli karpanoja <Pauli Karpanoja (pauli.karpanoja@solita.fi)>
2013-01-07 19:53:06 +0200

    UI-test for listing municipalities

--- municipalityinitiative-web/src/test/java/fi/om/municipalityinitiative/web/InitiativeCreateWebTest.java
+++ municipalityinitiative-web/src/test/java/fi/om/municipalityinitiative/web/InitiativeCreateWebTest.java
@@ -1,11 +1,33 @@
 package fi.om.municipalityinitiative.web;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 public class InitiativeCreateWebTest extends NEWWebTestBase {
 
     @Test
     public void municipalities_are_listed() {
-        //open(urls.createNew());
+
+        newTestHelper.createTestMunicipality("Korso");
+        newTestHelper.createTestMunicipality("Helsinki");
+
+        open(urls.createNew());
+
+        WebElement selectBox = getSelectByLabel("Valitse kunta");
+        List<WebElement> optionValues = selectBox.findElements(By.tagName("option"));
+
+        // First is empty, start from second
+        assertThat(optionValues.get(1).getText(), is("Helsinki"));
+        assertThat(optionValues.get(2).getText(), is("Korso"));
+    }
+
+    private WebElement getSelectByLabel(String labelText) {
+        return driver.findElement(By.xpath("//label[contains(normalize-space(text()), '" + labelText + "')]/following-sibling::select"));
     }
 }
