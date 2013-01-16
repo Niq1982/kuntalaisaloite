1005c3ae2d489522ee77e971e6d61f7e5a01cf3c
Pauli Karpanoja (pauli karpanoja <Pauli Karpanoja (pauli.karpanoja@solita.fi)>
2013-01-16 18:26:09 +0200

    Tests for create and get initiative rewrited and moved under service

--- municipalityinitiative-web/src/test/java/fi/om/municipalityinitiative/dao/JdbcMunicipalityInitiativeDaoTest.java
+++ municipalityinitiative-web/src/test/java/fi/om/municipalityinitiative/dao/JdbcMunicipalityInitiativeDaoTest.java
@@ -3,10 +3,8 @@ package fi.om.municipalityinitiative.dao;
 import fi.om.municipalityinitiative.conf.NEWIntegrationTestConfiguration;
 import fi.om.municipalityinitiative.newdao.MunicipalityInitiativeDao;
 import fi.om.municipalityinitiative.newdto.MunicipalityInfo;
-import fi.om.municipalityinitiative.newdto.MunicipalityInitiativeCreateDto;
 import fi.om.municipalityinitiative.newdto.MunicipalityInitiativeInfo;
 import fi.om.municipalityinitiative.newweb.MunicipalityInitiativeSearch;
-import fi.om.municipalityinitiative.sql.QMunicipalityInitiative;
 import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,10 +15,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import javax.annotation.Resource;
 
 import java.util.List;
-import java.util.Random;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.Matchers.hasSize;
 import static org.junit.Assert.assertThat;
 
@@ -44,25 +40,7 @@ public class JdbcMunicipalityInitiativeDaoTest {
         testMunicipality.setId(testHelper.createTestMunicipality(testMunicipality.getName()));
     }
 
-    @Test
-    public void testCreateASd() {
-        testHelper.createTestInitiative(testMunicipality.getId(), "jokunimi");
-    }
-
-
-    @Test
-    public void create() {
-        municipalityInitiativeDao.create(createDto());
-        assertThat(testHelper.countAll(QMunicipalityInitiative.municipalityInitiative), is(1L));
-    }
-
-    @Test
-    public void create_and_get() {
-        Long initiativeId = testHelper.createTestInitiative(testMunicipality.getId(), "Some name");
-
-        MunicipalityInitiativeInfo get = municipalityInitiativeDao.getById(initiativeId);
-        assertThat(1, Matchers.is(2));
-    }
+    // Create and get are tested at MunicipalityInitiativeServiceIntegrationTests
 
     @Test
     public void find_returns_all() {
@@ -116,33 +94,4 @@ public class JdbcMunicipalityInitiativeDaoTest {
 
     }
 
-    private MunicipalityInitiativeCreateDto createDto() {
-        MunicipalityInitiativeCreateDto dto = new MunicipalityInitiativeCreateDto();
-
-        dto.name = "initiativename"+randomString();
-        dto.proposal = "proposal"+randomString();
-        dto.municipalityId = testMunicipality.getId();
-
-        dto.contactAddress = "address"+randomString();
-        dto.contactName = "contactname"+randomString();
-        dto.contactEmail = "email"+randomString();
-        dto.contactPhone = "phone"+randomString();
-        return dto;
-    }
-
-    private static String randomString() {
-        return String.valueOf(new Random().nextLong());
-    }
-
-    private void assertCreateAndGetDtos(MunicipalityInitiativeCreateDto create, MunicipalityInitiativeInfo get) {
-        assertThat(get.getProposal(), is(create.proposal));
-        assertThat(get.getName(), is(create.name));
-        assertThat(get.getContactName(), is(create.contactName));
-        assertThat(get.getContactPhone(), is(create.contactPhone));
-        assertThat(get.getContactEmail(), is(create.contactEmail));
-        assertThat(get.getContactAddress(), is(create.contactAddress));
-        assertThat(get.getMunicipalityName(), is(testMunicipality.getName()));
-        assertThat(get.getCreateTime(), is(notNullValue()));
-        assertThat(get.getId(), is(notNullValue()));
-    }
 }
