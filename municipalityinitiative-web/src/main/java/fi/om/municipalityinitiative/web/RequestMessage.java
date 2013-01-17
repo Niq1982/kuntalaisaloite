34840a46f8e7e1021278423d0e16025d343e24bd
Pauli Karpanoja (pauli karpanoja <Pauli Karpanoja (pauli.karpanoja@solita.fi)>
2013-01-17 12:44:30 +0200

    Removed unused RequestMessages

--- municipalityinitiative-web/src/main/java/fi/om/municipalityinitiative/web/RequestMessage.java
+++ municipalityinitiative-web/src/main/java/fi/om/municipalityinitiative/web/RequestMessage.java
@@ -1,6 +1,6 @@
 package fi.om.municipalityinitiative.web;
+
 import static fi.om.municipalityinitiative.web.RequestMessageType.SUCCESS;
-import static fi.om.municipalityinitiative.web.RequestMessageType.WARNING;
 public enum RequestMessage {
     // Success messages - Municipality
     SAVE_AND_SEND (SUCCESS),
@@ -8,29 +8,7 @@ public enum RequestMessage {
     
     // Success messages
     //SAVE (SUCCESS),
-    SEND_INVITATIONS (SUCCESS),
-    SAVE_AND_SEND_INVITATIONS (SUCCESS, true),
-    ACCEPT_INVITATION (SUCCESS),
-    DECLINE_INVITATION (SUCCESS),
-    SEND_TO_OM (SUCCESS),
-    ACCEPT_BY_OM (SUCCESS),
-    REJECT_BY_OM (SUCCESS),
-    CONFIRM_CURRENT_AUTHOR (SUCCESS),
-    DELETE_CURRENT_AUTHOR (SUCCESS),
-    CONFIRM_VOTE (SUCCESS, true),
-    SEND_TO_VRK (SUCCESS),
-    SAVE_VRK_RESOLUTION (SUCCESS),
-    REMOVE_SUPPORT_VOTES (SUCCESS),
-    
-    // Warnings
-    SEND_INVITATIONS_FAILED (WARNING),
-    OPEN_INVITATION_FAILED (WARNING),
-    ALREADY_VOTED (WARNING),
-    VOTING_NOT_ALLOWED (WARNING),
-    ACCEPTING_INVITATIONS_NOT_ALLOWED (WARNING), 
-    CONFIRM_VOTE_UNCHECKED (WARNING), 
-    CONFIRM_REMOVAL_UNCHECKED (WARNING),
-    ADULT_REQUIRED_AS_AUTHOR (WARNING)
+
     ;
     
     private final boolean modal;
