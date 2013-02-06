253ae6e88878513bd727b46a8db0072e9762a6e2
Pauli Karpanoja (pauli karpanoja <Pauli Karpanoja (pauli.karpanoja@solita.fi)>
2013-02-06 14:53:11 +0200

    Emails are sent also when sending collectable initiative

--- municipalityinitiative-web/src/main/java/fi/om/municipalityinitiative/newweb/MunicipalityInitiativeViewController.java
+++ municipalityinitiative-web/src/main/java/fi/om/municipalityinitiative/newweb/MunicipalityInitiativeViewController.java
@@ -139,7 +139,7 @@ public class MunicipalityInitiativeViewController extends BaseController {
                                 BindingResult bindingResult,Model model, Locale locale, HttpServletRequest request) {
        
         if (validationService.validationSuccessful(sendToMunicipalityDto, bindingResult, model)) {
-            initiativeService.sendToMunicipality(initiativeId, "0000000000111111111122222222223333333333");
+            initiativeService.sendToMunicipality(initiativeId, "0000000000111111111122222222223333333333", locale);
             Urls urls = Urls.get(locale);
             return redirectWithMessage(urls.view(initiativeId),RequestMessage.SEND, request); 
         }
