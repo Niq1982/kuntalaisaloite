ba2097ec29db2cd3eed3f9a8633c5a1793d9c4e9
Pauli Karpanoja (pauli karpanoja <Pauli Karpanoja (pauli.karpanoja@solita.fi)>
2013-03-12 10:19:48 +0200

    Refactored

--- municipalityinitiative-web/src/test/java/fi/om/municipalityinitiative/newweb/JsonTest.java
+++ municipalityinitiative-web/src/test/java/fi/om/municipalityinitiative/newweb/JsonTest.java
@@ -1,8 +1,8 @@
 package fi.om.municipalityinitiative.newweb;
 
+import com.fasterxml.jackson.databind.MapperFeature;
 import com.google.common.base.Joiner;
 import fi.om.municipalityinitiative.json.JsonJokuParseri;
-import fi.om.municipalityinitiative.util.IntendedStringComparator;
 import fi.om.municipalityinitiative.util.Locales;
 import org.junit.Before;
 import org.junit.Test;
@@ -12,7 +12,6 @@ import org.springframework.ui.ExtendedModelMap;
 import javax.servlet.http.HttpServletRequest;
 
 import java.io.IOException;
-import java.util.Collections;
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -32,6 +31,7 @@ public class JsonTest {
     @Before
     public void setup() throws IOException {
         apiController.jsonConverter = jsonConverter;
+        jsonConverter.getObjectMapper().enable(MapperFeature.SORT_PROPERTIES_ALPHABETICALLY);
         model = new ExtendedModelMap();
         apiController.api(model, Locales.LOCALE_FI, mock(HttpServletRequest.class));
 
@@ -41,44 +41,65 @@ public class JsonTest {
     public void initiative_details_have_not_changed() throws IOException {
         List<JsonJokuParseri.IndentedString> initiatives = getJsonDataListFromModel("initiative");
         String join = joinAsString(initiatives);
-        assertThat(join, is("\"authorName\":\"Teemu Teekkari\",\n" +
+        System.out.println(join);
+        assertThat(join, is("{\n" +
+                "\"authorName\":\"Teemu Teekkari\",\n" +
                 "\"collectable\":true,\n" +
                 "\"createTime\":\"2010-01-01\",\n" +
-                "\"franchise\":true,\n" +
-                "\"franchise\":{\n" +
-                "\"homeMunicipality\":{\n" +
-                "\"id\":1,\n" +
-                "\"id\":1,\n" +
                 "\"id\":1,\n" +
                 "\"municipality\":{\n" +
+                "\"id\":1,\n" +
+                "\"nameFi\":\"Tampere\",\n" +
+                "\"nameSv\":\"Tammerfors\"\n" +
+                "},\n" +
                 "\"name\":\"Koirat pois lähiöistä\",\n" +
-                "\"name\":\"Teemu Teekkari\",\n" +
-                "\"nameFi\":\"Tampere\",\n" +
-                "\"nameFi\":\"Tampere\",\n" +
-                "\"nameSv\":\"Tammerfors\"\n" +
-                "\"nameSv\":\"Tammerfors\"\n" +
-                "\"noFranchise\":{\n" +
                 "\"participantCount\":{\n" +
-                "\"participateDate\":\"2010-01-01\",\n" +
+                "\"franchise\":{\n" +
                 "\"privateNames\":10,\n" +
-                "\"privateNames\":12,\n" +
-                "\"proposal\":\"Kakkaa on joka paikassa\",\n" +
-                "\"publicNames\":0,\n" +
                 "\"publicNames\":1,\n" +
-                "\"publicParticipants\":[\n" +
-                "\"sentTime\":null\n" +
                 "\"total\":11\n" +
+                "},\n" +
+                "\"noFranchise\":{\n" +
+                "\"privateNames\":12,\n" +
+                "\"publicNames\":0,\n" +
                 "\"total\":12\n" +
+                "},\n" +
                 "\"total\":23\n" +
+                "},\n" +
+                "\"proposal\":\"Kakkaa on joka paikassa\",\n" +
+                "\"publicParticipants\":[\n" +
+                "{\n" +
+                "\"franchise\":true,\n" +
+                "\"homeMunicipality\":{\n" +
+                "\"id\":1,\n" +
+                "\"nameFi\":\"Tampere\",\n" +
+                "\"nameSv\":\"Tammerfors\"\n" +
+                "},\n" +
+                "\"name\":\"Teemu Teekkari\",\n" +
+                "\"participateDate\":\"2010-01-01\"\n" +
+                "}]\n" +
                 ",\n" +
+                "\"sentTime\":null\n" +
+                "}"));
+    }
+
+    @Test
+    public void initiative_list_has_not_changed() throws IOException {
+        List<JsonJokuParseri.IndentedString> initiatives = getJsonDataListFromModel("initiativeList");
+        String join = joinAsString(initiatives);
+        assertThat(join, is("[\n" +
                 "{\n" +
-                "{\n" +
-                "}\n" +
-                "}\n" +
+                "\"collectable\":true,\n" +
+                "\"createTime\":\"2012-12-01\",\n" +
+                "\"id\":1,\n" +
+                "\"municipality\":{\n" +
+                "\"id\":1,\n" +
+                "\"nameFi\":\"Tampere\",\n" +
+                "\"nameSv\":\"Tammerfors\"\n" +
                 "},\n" +
-                "},\n" +
-                "},\n" +
-                "},\n" +
+                "\"name\":\"Koirat pois lähiöistä\",\n" +
+                "\"participantCount\":2,\n" +
+                "\"sentTime\":\"2012-12-24\"\n" +
                 "}]"));
     }
 
@@ -87,18 +108,16 @@ public class JsonTest {
         List<JsonJokuParseri.IndentedString> municipalities = getJsonDataListFromModel("municipalities");
         String join = joinAsString(municipalities);
 
-        assertThat(join, is("\"id\":1,\n" +
+        assertThat(join, is("[\n" +
+                "{\n" +
+                "\"id\":1,\n" +
                 "\"nameFi\":\"Tampere\",\n" +
                 "\"nameSv\":\"Tammerfors\"\n" +
-                "[\n" +
-                "{\n" +
                 "}]"));
     }
 
     private List<JsonJokuParseri.IndentedString> getJsonDataListFromModel(String modelAttributeName) {
-        List<JsonJokuParseri.IndentedString> indentedStrings = (List<JsonJokuParseri.IndentedString>) model.get(modelAttributeName);
-        Collections.sort(indentedStrings, new IntendedStringComparator());
-        return indentedStrings;
+        return (List<JsonJokuParseri.IndentedString>) model.get(modelAttributeName);
     }
 
     private String joinAsString(List<JsonJokuParseri.IndentedString> initiatives) {
