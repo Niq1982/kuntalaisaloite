89e4b1472c77b5072b2876701e39d91296ef11df
Pauli Karpanoja (pauli karpanoja <Pauli Karpanoja (pauli.karpanoja@solita.fi)>
2013-03-18 19:44:34 +0200

    Fixed municipality id:s

--- municipalityinitiative-web/etc/muninit_schema/02_populate_municipalities.sql
+++ municipalityinitiative-web/etc/muninit_schema/02_populate_municipalities.sql
@@ -1,7 +1,7 @@
 insert into municipality (id, name, name_sv) values (1, 'Helsinki','Helsingfors');
-insert into municipality (id, name, name_sv) values (1, 'Tampere','Tammerfors');
-insert into municipality (id, name, name_sv) values (1, 'Tuusula','Tuusby');
-insert into municipality (id, name, name_sv) values (1, 'Hämeenlinna','Tavastehus');
-insert into municipality (id, name, name_sv) values (1, 'Vantaa','Vanda');
-insert into municipality (id, name, name_sv) values (1, 'Kirkkonummi','Kyrkslätt');
-insert into municipality (id, name, name_sv) values (1, 'Turku','Åbo');
+insert into municipality (id, name, name_sv) values (2, 'Tampere','Tammerfors');
+insert into municipality (id, name, name_sv) values (3, 'Tuusula','Tuusby');
+insert into municipality (id, name, name_sv) values (4, 'Hämeenlinna','Tavastehus');
+insert into municipality (id, name, name_sv) values (5, 'Vantaa','Vanda');
+insert into municipality (id, name, name_sv) values (6, 'Kirkkonummi','Kyrkslätt');
+insert into municipality (id, name, name_sv) values (7, 'Turku','Åbo');
