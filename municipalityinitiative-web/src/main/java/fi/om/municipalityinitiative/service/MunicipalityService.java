63f9cff4a164dc4df7d8a30966fb62518b2afa1b
Pauli Karpanoja (pauli karpanoja <Pauli Karpanoja (pauli.karpanoja@solita.fi)>
2013-04-10 14:42:45 +0300

    optimized imports

--- municipalityinitiative-web/src/main/java/fi/om/municipalityinitiative/service/MunicipalityService.java
+++ municipalityinitiative-web/src/main/java/fi/om/municipalityinitiative/service/MunicipalityService.java
@@ -1,9 +1,7 @@
 package fi.om.municipalityinitiative.service;
 
-import com.google.common.collect.Lists;
 import fi.om.municipalityinitiative.newdao.MunicipalityDao;
 import fi.om.municipalityinitiative.newdto.service.Municipality;
-import fi.om.municipalityinitiative.newdto.ui.MunicipalityInfo;
 import fi.om.municipalityinitiative.util.Locales;
 
 import javax.annotation.Resource;
