fbf2dc2fb4a796e40d79748db6bb3738d99c88ca
Pauli Karpanoja (pauli karpanoja <Pauli Karpanoja (pauli.karpanoja@solita.fi)>
2013-04-17 11:50:00 +0300

    Controller refactoring: search-page

--- municipalityinitiative-web/src/main/java/fi/om/municipalityinitiative/newweb/ViewGenerator.java
+++ municipalityinitiative-web/src/main/java/fi/om/municipalityinitiative/newweb/ViewGenerator.java
@@ -2,18 +2,23 @@ package fi.om.municipalityinitiative.newweb;
 
 import com.apple.jobjc.appkit.NSMenuItem;
 import com.google.common.collect.Maps;
+import fi.om.municipalityinitiative.dto.InitiativeCounts;
+import fi.om.municipalityinitiative.newdto.InitiativeSearch;
 import fi.om.municipalityinitiative.newdto.service.Municipality;
-import fi.om.municipalityinitiative.newdto.ui.InitiativeViewInfo;
-import fi.om.municipalityinitiative.newdto.ui.ParticipantCount;
-import fi.om.municipalityinitiative.newdto.ui.ParticipantUICreateDto;
-import fi.om.municipalityinitiative.newdto.ui.Participants;
+import fi.om.municipalityinitiative.newdto.ui.*;
+import fi.om.municipalityinitiative.util.Maybe;
+import fi.om.municipalityinitiative.web.SearchParameterQueryString;
 import fi.om.municipalityinitiative.web.Views;
 import org.springframework.ui.Model;
 
 import java.util.List;
 import java.util.Map;
 
+import static fi.om.municipalityinitiative.web.Views.SEARCH_VIEW;
+
 public class ViewGenerator {
+
+    private static final String ALT_URI_ATTR = "altUri";
 
     private final String viewName;
     private final Map<String, Object> modelAttributes;
@@ -39,15 +44,34 @@ public class ViewGenerator {
         );
     }
 
-    public String view(Model model) {
+    public String view(Model model, String alternativeURL) {
         for (Map.Entry<String, Object> stringObjectEntry : modelAttributes.entrySet()) {
             model.addAttribute(stringObjectEntry.getKey(), stringObjectEntry.getValue());
         }
+        model.addAttribute(ALT_URI_ATTR, alternativeURL);
         return viewName;
     }
 
     public static ViewGenerator singleView(InitiativeViewInfo initiativeInfo) {
         return new ViewGenerator(Views.PUBLIC_SINGLE_VIEW,new AttributeBuilder().add("initiative", initiativeInfo).build());
+    }
+
+    public static ViewGenerator searchView(List<InitiativeListInfo> initiatives,
+                                           List<Municipality> municipalities,
+                                           InitiativeSearch currentSearch,
+                                           SearchParameterQueryString queryString,
+                                           Maybe<Municipality> currentMunicipality,
+                                           InitiativeCounts initiativeCounts) {
+        return new ViewGenerator(SEARCH_VIEW,
+                new AttributeBuilder()
+                        .add("initiatives", initiatives)
+                        .add("municipalities", municipalities)
+                        .add("currentSearch", currentSearch)
+                        .add("queryString", queryString)
+                        .add("currentMunicipality", currentMunicipality)
+                        .add("initiativeCounts", initiativeCounts)
+                        .build()
+        );
     }
 
     private static class AttributeBuilder {
