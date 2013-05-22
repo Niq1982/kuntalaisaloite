887617cdca6c8dd02f2963a79d28ccf58ecf6d10
Pauli Karpanoja (pauli karpanoja <Pauli Karpanoja (pauli.karpanoja@solita.fi)>
2013-05-22 09:05:06 +0300

    Refactored

--- municipalityinitiative-web/src/main/java/fi/om/municipalityinitiative/service/ImageFinder.java
+++ municipalityinitiative-web/src/main/java/fi/om/municipalityinitiative/service/ImageFinder.java
@@ -1,15 +1,14 @@
 package fi.om.municipalityinitiative.service;
 
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
 
 public interface ImageFinder {
+
     List<FileImageFinder.FileJson>  getImages();
 
-    @Cacheable("infoTextImages")
     FileImageFinder.FileInfo getFile(String fileName, String version) throws IOException;
 
     void validateAndSaveFile(MultipartFile multipartFile) throws IOException;
