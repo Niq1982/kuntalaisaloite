3a19ec09298a34655c6852005a6882fe096ed523
Pauli Karpanoja (pauli karpanoja <Pauli Karpanoja (pauli.karpanoja@solita.fi)>
2013-05-28 09:21:14 +0300

    Added tests for search-limits

--- municipalityinitiative-web/src/test/java/fi/om/municipalityinitiative/dto/InitiativeSearchTest.java
+++ municipalityinitiative-web/src/test/java/fi/om/municipalityinitiative/dto/InitiativeSearchTest.java
@@ -1,7 +1,11 @@
 package fi.om.municipalityinitiative.dto;
 
 import fi.om.municipalityinitiative.util.ReflectionTestUtils;
+import fi.om.municipalityinitiative.web.Urls;
 import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class InitiativeSearchTest {
 
@@ -13,5 +17,20 @@ public class InitiativeSearchTest {
 
         ReflectionTestUtils.assertNoNullFields(copy);
         ReflectionTestUtils.assertReflectionEquals(original, copy);
+    }
+
+    @Test
+    public void has_max_limit_value() {
+        assertThat(new InitiativeSearch().setLimit(100000).getLimit(), is(Urls.MAX_INITIATIVE_SEARCH_LIMIT));
+    }
+
+    @Test
+    public void limit_is_what_is_given_if_not_max_limit() {
+        assertThat(new InitiativeSearch().setLimit(10).getLimit(), is(10));
+    }
+
+    @Test
+    public void limit_is_default_if_not_set() {
+        assertThat(new InitiativeSearch().getLimit(), is(Urls.DEFAULT_INITIATIVE_SEARCH_LIMIT));
     }
 }
