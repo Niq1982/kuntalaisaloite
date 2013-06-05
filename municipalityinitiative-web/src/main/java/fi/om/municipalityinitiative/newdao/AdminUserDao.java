acfedc5300cabdb09dff62ba6e42d6c5c22eaaa7
Pauli Karpanoja (pauli karpanoja <Pauli Karpanoja (pauli.karpanoja@solita.fi)>
2013-06-05 15:58:35 +0300

    Moved classes and added test class for FileImageFinder

--- municipalityinitiative-web/src/main/java/fi/om/municipalityinitiative/newdao/AdminUserDao.java
+++
@@ -1,7 +0,0 @@
-package fi.om.municipalityinitiative.newdao;
-
-import fi.om.municipalityinitiative.dto.user.User;
-
-public interface AdminUserDao {
-    public User getUser(String userName, String password);
-}
