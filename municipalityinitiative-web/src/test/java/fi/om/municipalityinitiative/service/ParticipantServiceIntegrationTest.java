cf7ff22df5138cf91868c1432fca31e6623571ca
Pauli Karpanoja (pauli karpanoja <Pauli Karpanoja (pauli.karpanoja@solita.fi)>
2013-07-09 16:00:22 +0300

    Added test for author flag when getting normal participantlist

--- municipalityinitiative-web/src/test/java/fi/om/municipalityinitiative/service/ParticipantServiceIntegrationTest.java
+++ municipalityinitiative-web/src/test/java/fi/om/municipalityinitiative/service/ParticipantServiceIntegrationTest.java
@@ -1,10 +1,14 @@
 package fi.om.municipalityinitiative.service;
 
 import fi.om.municipalityinitiative.dao.TestHelper;
+import fi.om.municipalityinitiative.dto.ui.ParticipantListInfo;
 import org.junit.Test;
 
 import javax.annotation.Resource;
 
+import java.util.List;
+
+import static fi.om.municipalityinitiative.util.TestUtil.precondition;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasSize;
@@ -55,6 +59,18 @@ public class ParticipantServiceIntegrationTest extends ServiceIntegrationTestBas
     public void findAllParticipants_for_normal_initiative() {
         Long initiativeId = createNormalInitiativeWithAuthor();
         assertThat(participantService.findAllParticipants(initiativeId, TestHelper.authorLoginUserHolder), hasSize(1));
+    }
+
+    @Test
+    public void findPublicParticipants_for_normal_initiative_sets_author_flag_true_if_author() {
+        Long initiativeId = createNormalInitiativeWithAuthor();
+        testHelper.createDefaultParticipant(new TestHelper.AuthorDraft(initiativeId, testMunicipalityId).withPublicName(true));
+
+        List<ParticipantListInfo> publicParticipants = participantService.findPublicParticipants(initiativeId);
+        precondition(publicParticipants, hasSize(2));
+
+        assertThat(publicParticipants.get(0).isAuthor(), is(false));
+        assertThat(publicParticipants.get(1).isAuthor(), is(true));
     }
 
     private Long createNormalInitiativeWithAuthor() {
