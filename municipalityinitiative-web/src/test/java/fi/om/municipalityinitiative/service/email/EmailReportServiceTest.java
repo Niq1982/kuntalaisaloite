4da34bb1b7e85354ec14de513ba9d680df70be05
Pauli Karpanoja <pauli.karpanoja@solita.fi>
2014-10-13 19:25:18 +0300

    Added test

--- municipalityinitiative-web/src/test/java/fi/om/municipalityinitiative/service/email/EmailReportServiceTest.java
+++ municipalityinitiative-web/src/test/java/fi/om/municipalityinitiative/service/email/EmailReportServiceTest.java
@@ -2,7 +2,12 @@ package fi.om.municipalityinitiative.service.email;
 
 import fi.om.municipalityinitiative.conf.IntegrationTestFakeEmailConfiguration;
 import fi.om.municipalityinitiative.dao.TestHelper;
+import fi.om.municipalityinitiative.dto.service.EmailDto;
 import fi.om.municipalityinitiative.util.InitiativeState;
+import fi.om.municipalityinitiative.util.InitiativeType;
+import fi.om.municipalityinitiative.util.Locales;
+import fi.om.municipalityinitiative.util.RandomHashGenerator;
+import fi.om.municipalityinitiative.web.Urls;
 import org.joda.time.DateTime;
 import org.junit.Before;
 import org.junit.Test;
@@ -13,6 +18,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import javax.annotation.Resource;
 
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasSize;
 
@@ -28,9 +34,12 @@ public class EmailReportServiceTest {
 
     private Long testMunicipality;
 
+    private Urls urls;
+
     @Before
     public void setup() {
         testHelper.dbCleanup();
+        urls = Urls.get(Locales.LOCALE_FI);
         testMunicipality = testHelper.createTestMunicipality("Some municipality");
     }
 
@@ -45,7 +54,30 @@ public class EmailReportServiceTest {
                 .toInitiativeDraft());
 
         emailReportService.sendReportEmailsForInitiativesAcceptedButNotPublished();
-        assertThat(testHelper.getSingleQueuedEmail().getSubject(), containsString("Aloitteesi odottaa vielä julkaisua"));
+        EmailDto singleQueuedEmail = testHelper.getSingleQueuedEmail();
+        assertThat(singleQueuedEmail.getSubject(), containsString("Aloitteesi odottaa vielä julkaisua"));
+        assertThat(singleQueuedEmail.getBodyHtml(), containsString(urls.loginAuthor(RandomHashGenerator.getPrevious())));
+        assertThat(singleQueuedEmail.getRecipientsAsString(), is("author@example.com"));
+
+        emailReportService.sendReportEmailsForInitiativesAcceptedButNotPublished();
+        assertThat(testHelper.findQueuedEmails(), hasSize(1)); // Is not sent again
+    }
+
+    @Test
+    public void sends_accepted_but_not_published_for_verified_initiatives() {
+        Long verifiedInitiative = testHelper.createVerifiedInitiative(new TestHelper.InitiativeDraft(testMunicipality)
+                .withState(InitiativeState.ACCEPTED)
+                .withStateTime(new DateTime().minusDays(15))
+                .withType(InitiativeType.COLLABORATIVE_COUNCIL)
+                .applyAuthor()
+                .withParticipantEmail("author@example.com")
+                .toInitiativeDraft());
+
+        emailReportService.sendReportEmailsForInitiativesAcceptedButNotPublished();
+        EmailDto singleQueuedEmail = testHelper.getSingleQueuedEmail();
+        assertThat(singleQueuedEmail.getRecipientsAsString(), is("author@example.com"));
+        assertThat(singleQueuedEmail.getSubject(), containsString("Aloitteesi odottaa vielä julkaisua"));
+        assertThat(singleQueuedEmail.getBodyHtml(), containsString(urls.loginToManagement(verifiedInitiative)));
 
         emailReportService.sendReportEmailsForInitiativesAcceptedButNotPublished();
         assertThat(testHelper.findQueuedEmails(), hasSize(1)); // Is not sent again
