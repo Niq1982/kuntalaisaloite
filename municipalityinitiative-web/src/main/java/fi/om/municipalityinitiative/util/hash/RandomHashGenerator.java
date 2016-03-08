0f9c5b52b63e13c9a0989addd1e086bb8ab7cbca
Pauli Kärpänoja <pauli.karpanoja@solita.fi>
2016-03-08 10:49:17 +0200

    Improvements to randomhashgenerator

--- municipalityinitiative-web/src/main/java/fi/om/municipalityinitiative/util/hash/RandomHashGenerator.java
+++ municipalityinitiative-web/src/main/java/fi/om/municipalityinitiative/util/hash/RandomHashGenerator.java
@@ -1,43 +1,42 @@
 package fi.om.municipalityinitiative.util.hash;
 
 import java.security.SecureRandom;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 public abstract class RandomHashGenerator {
 
+    private static final Object lock = new Object();
+
     private static String previous;
 
-    private static final String AB = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZqwertyuiopasdfghjklzxcvbnm";
+    private static final String ALLOWED_CHARACTERS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZqwertyuiopasdfghjklzxcvbnm";
 
     private static SecureRandom rnd = new SecureRandom();
 
-    private static AtomicInteger increment = new AtomicInteger(0);
+    private static AtomicLong increment = new AtomicLong(0);
 
-    public static void main(String[] params) {
-        for (int i = 0; i < 1000; ++i) {
-            System.out.println(randomString(40));
+    public static String randomString( int len ) {
+        synchronized (lock) {
+            if (increment.incrementAndGet() % 100 == 0) {
+                rnd.setSeed(rnd.generateSeed(1024));
+            }
+
+            StringBuilder builder = new StringBuilder( len );
+            for( int i = 0; i < len; i++ ) {
+                builder.append( ALLOWED_CHARACTERS.charAt( rnd.nextInt(ALLOWED_CHARACTERS.length()) ) );
+            }
+
+            previous = builder.toString();
+            return previous;
         }
-    }
-
-    public synchronized static String randomString( int len )
-    {
-        if (increment.incrementAndGet() % 100 == 0) {
-            rnd.setSeed(rnd.generateSeed(1024));
-        }
-
-        StringBuilder sb = new StringBuilder( len );
-        for( int i = 0; i < len; i++ )
-            sb.append( AB.charAt( rnd.nextInt(AB.length()) ) );
-        previous = sb.toString();
-        return previous;
     }
 
     public static String longHash() {
-        return RandomHashGenerator.randomString(40);
+        return randomString(40);
     }
 
     public static String shortHash() {
-        return RandomHashGenerator.randomString(20);
+        return randomString(20);
     }
 
     // For tests
