d85593c32c2d27da482d9ff84221f7318261a647
Pauli Kärpänoja <pauli.karpanoja@solita.fi>
2016-12-05 09:58:12 +0200

    Remove dead classes

--- municipalityinitiative-web/src/main/java/fi/om/municipalityinitiative/conf/AuthenticationFailureHandler.java
+++
@@ -1,6 +0,0 @@
-package fi.om.municipalityinitiative.conf;
-
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
-
-public class AuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
-}
