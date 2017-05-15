170129cad7b7ab0a5366e2166c64360715804055
Pauli Kärpänoja <pauli.karpanoja@solita.fi>
2017-05-15 16:52:38 +0300

    Add denormalized participant_count_citizen column and denormalize old data

---
+++ municipalityinitiative-web/src/main/resources/db/migration/V14__participant_count_citizen.sql
@@ -0,0 +1,24 @@
+ALTER TABLE municipality_initiative
+  ADD COLUMN participant_count_citizen BIGINT NOT NULL DEFAULT 0;
+
+-- Denormalize from verified participants
+update municipality_initiative
+  set participant_count_citizen = subselect.c
+from
+  (select count(*) as c,
+     initiative_id as initiative_id
+   from verified_participant vp inner join municipality_initiative i on (i.id = vp.initiative_id)
+   where i.municipality_id = vp.municipality_id group by initiative_id) as subselect
+where
+  municipality_initiative.id = subselect.initiative_id;
+
+-- Denormalize from normal participants
+update municipality_initiative
+set participant_count_citizen = participant_count_citizen + subselect.c
+from
+  (select count(*) as c,
+          municipality_initiative_id as initiative_id
+   from participant vp inner join municipality_initiative i on (i.id = vp.municipality_initiative_id)
+   where i.municipality_id = vp.municipality_id group by initiative_id) as subselect
+where
+  municipality_initiative.id = subselect.initiative_id;
