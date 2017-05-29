730d8e0e0ff198e8aa90fb1adcac58a1f15691f7
Pauli Kärpänoja <pauli.karpanoja@solita.fi>
2017-05-29 20:24:36 +0300

    Change to migration when denormalizing citizenSupportCount: require confirmation_code to be null for normal participants

--- municipalityinitiative-web/src/main/resources/db/migration/V15__participant_count_citizen.sql
+++ municipalityinitiative-web/src/main/resources/db/migration/V15__participant_count_citizen.sql
@@ -19,6 +19,8 @@ from
   (select count(*) as c,
           municipality_initiative_id as initiative_id
    from participant vp inner join municipality_initiative i on (i.id = vp.municipality_initiative_id)
-   where i.municipality_id = vp.municipality_id group by initiative_id) as subselect
+   where i.municipality_id = vp.municipality_id
+     and confirmation_code is null
+   group by initiative_id) as subselect
 where
   municipality_initiative.id = subselect.initiative_id;
